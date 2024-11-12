82cb537ea8b5174d2b22dd1a75fe7dbdc62ee0b9
skoky <skokys@gmail.com>
2024-11-12 16:23:27 +0100

    tests saving output to file

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import json
+import os
 import wave
 
 import numpy as np
@@ -12,6 +14,7 @@ def test_mel_spectogram():
     assert mels.shape == (397, 32)
     assert mels.max() == -7.999999
     assert mels.min() == -7.999999
+    save_result(mels, "mels_from_empty_array.txt")
 
 
 def test_embeddings():
@@ -20,6 +23,7 @@ def test_embeddings():
     assert embeddings.shape == (41, 96)
     assert embeddings.max() == 38.666107
     assert embeddings.min() == -37.116394
+    save_result(embeddings, "embeddings_from_empty_array.txt")
 
 
 def test_audio_preproc():
@@ -29,6 +33,8 @@ def test_audio_preproc():
     assert result
     assert a.feature_buffer.shape == (42, 96)
     assert a.melspectrogram_buffer.shape == (81, 32)
+    save_result(a.feature_buffer, "audio_feature_buffer_from_empty_array.txt")
+    save_result(a.melspectrogram_buffer, "audio_melspectogram_from_empty_array.txt")
 
 
 def test_model():
@@ -49,15 +55,29 @@ def test_detection_from_wav():
     filedata = read_wav_int16('hey_jarvis.wav')
     model = Model(["hey_jarvis_v0.1.onnx"])
     chunks = np.array_split(filedata, np.ceil(len(filedata) / CHUNK))
-    positive_predictions_count = 0
+    positive_predictions = []
     for chunk in chunks:
         prediction = model.predict(chunk)
         p = prediction['hey_jarvis_v0.1']
         if p > 0.9:
-            print(prediction)
-            positive_predictions_count += 1
+            positive_predictions.append(prediction)
 
-    assert 3 <= positive_predictions_count <= 5
+    assert 3 <= len(positive_predictions) <= 5
+    save_predictions(positive_predictions, "predictions_hey_jarvis_from_empty_array.txt")
+
+
+def test_detection_from_wav_negative():
+    filedata = read_wav_int16('negative.wav')
+    model = Model(["hey_jarvis_v0.1.onnx"])
+    chunks = np.array_split(filedata, np.ceil(len(filedata) / CHUNK))
+    positive_predictions = []
+    for chunk in chunks:
+        prediction = model.predict(chunk)
+        p = prediction['hey_jarvis_v0.1']
+        if p > 0.1:
+            positive_predictions.append(prediction)
+
+    assert len(positive_predictions) == 0
 
 
 def read_wav_int16(wav_file) -> np.ndarray:
@@ -66,3 +86,22 @@ def read_wav_int16(wav_file) -> np.ndarray:
         raw_bytes = wf.readframes(num_frames)
         samples = np.frombuffer(raw_bytes, dtype=np.int16)
         return samples
+
+
+def save_result(data: np.ndarray, filename):
+    os.makedirs("test_results", exist_ok=True)
+    np.savetxt(f'test_results/{filename}', data,
+               delimiter=',', fmt='%f',
+               header=f'Dim: {data.shape}')
+
+
+def save_predictions(data: list[dict], filename):
+    class FloatStringEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, np.float32):
+                return str(obj)
+            return super().default(obj)
+
+    os.makedirs("test_results", exist_ok=True)
+    with open(f'test_results/{filename}', "w") as file:
+        json.dump(data, file, indent=4, cls=FloatStringEncoder)
