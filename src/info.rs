be638d253d11161232285f1cd1e0452c62abf578
TachyonicBytes <support@tachyonicbytes.com>
2025-11-25 18:59:36 +0200

    feat: Add mycroft model

--- src/info.rs
+++ src/info.rs
@@ -1,9 +1,10 @@
-use crate::config::SpeechUnlockType::{OpenWakeWordAlexa};
+use crate::config::SpeechUnlockType;
 use crate::config::{UnlockConfig};
 use log::{debug};
 
 pub const OWW_CZ_NAME_AHOJ_HUGO: &str = "Český - Ahoj Hugo";
 pub const OWW_CZ_NAME_ALEXA: &str = "Český - Alexa";
+pub const OWW_CZ_NAME_HEY_MYCROFT: &str = "Český - Hey Mycroft";
 
 #[derive(Debug)]
 pub struct LanguageModel {
@@ -19,7 +20,8 @@ impl LanguageModel {
 
 pub fn get_trigger_phases(unlock_config: &UnlockConfig) -> Vec<String> {
     match unlock_config.unlock_type {
-        OpenWakeWordAlexa => vec!["Alexa".to_string()],
+        SpeechUnlockType::OpenWakeWordAlexa => vec!["Alexa".to_string()],
+        SpeechUnlockType::OpenWakeWordHeyMycroft => vec!["Hey Mycroft".to_string()],
     }
 }
 
@@ -27,7 +29,8 @@ pub fn set_unlock_model(language_model: &LanguageModel) -> Option<UnlockConfig> 
     let unlock_config = UnlockConfig::default(); // load_config(unlock_config_file.clone());
 
     let model_type = match language_model.name.as_str() {
-        OWW_CZ_NAME_ALEXA => OpenWakeWordAlexa,
+        OWW_CZ_NAME_ALEXA => SpeechUnlockType::OpenWakeWordAlexa,
+        OWW_CZ_NAME_HEY_MYCROFT => SpeechUnlockType::OpenWakeWordHeyMycroft,
         _ => {
             panic!("Unexpected language model {:?}", language_model);
         }
